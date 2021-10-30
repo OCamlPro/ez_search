933469378dd65bacceac50816be3f5fa6cb22787
lefessan <fabrice@lefessant.net>
2021-10-30 10:52:33 +0000

    Deploying to gh-pages from  @ 65c924b066a9064794a6fe61e35ef6d8c5a557e5 🚀

---
+++ sphinx/_sources/about.rst.txt
@@ -0,0 +1,11 @@
+About
+=====
+
+This is the description
+of the ez_search OCaml project
+
+
+Authors
+-------
+
+* Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
---
+++ sphinx/_static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'master (65c924b) (2021/10/30 10:52)',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
