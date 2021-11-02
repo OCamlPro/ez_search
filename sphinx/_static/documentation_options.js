8e1ea956e543e54f7962f84f82a2bfc4665f8a50
lefessan <fabrice@lefessant.net>
2021-11-02 15:56:15 +0000

    Deploying to gh-pages from  @ 29900408da1da049f4367521bf0e89d1beee2b8e 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (0826e72) (2021/10/30 21:31)',
+    VERSION: 'master (2990040) (2021/11/02 15:56)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
